910883a281772fd4d246d7893322a92ca8aaa570
Evgeny Gusarov <evgeny@stakewise.io>
2025-05-07 14:42:51 +0300

    Upd sw-utils 0.9.7

--- src/config/networks.py
+++ src/config/networks.py
@@ -1,91 +1,28 @@
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 
-from eth_typing import ChecksumAddress, HexStr
-from sw_utils import ConsensusFork
-from sw_utils.typings import Bytes32
-from web3 import Web3
+from sw_utils.networks import NETWORKS as BASE_NETWORKS
+from sw_utils.networks import BaseNetworkConfig
 
 MAINNET = 'mainnet'
 GNOSIS = 'gnosis'
-HOLESKY = 'holesky'
+HOODI = 'hoodi'
 CHIADO = 'chiado'
 
 GNO_NETWORKS = [GNOSIS, CHIADO]
 
 
 @dataclass
-class NetworkConfig:
-    CHAIN_ID: int
-    GENESIS_FORK_VERSION: bytes
-    GENESIS_VALIDATORS_ROOT: Bytes32
-    VALIDATORS_REGISTRY_CONTRACT_ADDRESS: ChecksumAddress
-
-    SHAPELLA_FORK_VERSION: bytes
-    SHAPELLA_EPOCH: int
-
-    @property
-    def SHAPELLA_FORK(self) -> ConsensusFork:
-        return ConsensusFork(
-            version=self.SHAPELLA_FORK_VERSION,
-            epoch=self.SHAPELLA_EPOCH,
-        )
+class NetworkConfig(BaseNetworkConfig):
+    pass
 
 
 NETWORKS = {
-    MAINNET: NetworkConfig(
-        CHAIN_ID=1,
-        GENESIS_FORK_VERSION=Web3.to_bytes(hexstr=HexStr('0x00000000')),
-        GENESIS_VALIDATORS_ROOT=Bytes32(
-            Web3.to_bytes(
-                hexstr=HexStr('0x4b363db94e286120d76eb905340fdd4e54bfe9f06bf33ff6cf5ad27f511bfe95')
-            )
-        ),
-        VALIDATORS_REGISTRY_CONTRACT_ADDRESS=Web3.to_checksum_address(
-            '0x00000000219ab540356cBB839Cbe05303d7705Fa'
-        ),
-        SHAPELLA_FORK_VERSION=Web3.to_bytes(hexstr=HexStr('0x03000000')),
-        SHAPELLA_EPOCH=194048,
-    ),
-    HOLESKY: NetworkConfig(
-        CHAIN_ID=17000,
-        GENESIS_FORK_VERSION=Web3.to_bytes(hexstr=HexStr('0x01017000')),
-        GENESIS_VALIDATORS_ROOT=Bytes32(
-            Web3.to_bytes(
-                hexstr=HexStr('0x9143aa7c615a7f7115e2b6aac319c03529df8242ae705fba9df39b79c59fa8b1')
-            )
-        ),
-        VALIDATORS_REGISTRY_CONTRACT_ADDRESS=Web3.to_checksum_address(
-            '0x4242424242424242424242424242424242424242'
-        ),
-        SHAPELLA_FORK_VERSION=Web3.to_bytes(hexstr=HexStr('0x04017000')),
-        SHAPELLA_EPOCH=256,
-    ),
+    MAINNET: NetworkConfig(**asdict(BASE_NETWORKS[MAINNET])),
+    HOODI: NetworkConfig(**asdict(BASE_NETWORKS[HOODI])),
     GNOSIS: NetworkConfig(
-        CHAIN_ID=100,
-        GENESIS_FORK_VERSION=Web3.to_bytes(hexstr=HexStr('0x00000064')),
-        GENESIS_VALIDATORS_ROOT=Bytes32(
-            Web3.to_bytes(
-                hexstr=HexStr('0xf5dcb5564e829aab27264b9becd5dfaa017085611224cb3036f573368dbb9d47')
-            )
-        ),
-        VALIDATORS_REGISTRY_CONTRACT_ADDRESS=Web3.to_checksum_address(
-            '0x0B98057eA310F4d31F2a452B414647007d1645d9'
-        ),
-        SHAPELLA_FORK_VERSION=Web3.to_bytes(hexstr=HexStr('0x03000064')),
-        SHAPELLA_EPOCH=648704,
+        **asdict(BASE_NETWORKS[GNOSIS]),
     ),
     CHIADO: NetworkConfig(
-        CHAIN_ID=10200,
-        GENESIS_FORK_VERSION=Web3.to_bytes(hexstr=HexStr('0x0000006f')),
-        GENESIS_VALIDATORS_ROOT=Bytes32(
-            Web3.to_bytes(
-                hexstr=HexStr('0x9d642dac73058fbf39c0ae41ab1e34e4d889043cb199851ded7095bc99eb4c1e')
-            )
-        ),
-        VALIDATORS_REGISTRY_CONTRACT_ADDRESS=Web3.to_checksum_address(
-            '0xb97036A26259B7147018913bD58a774cf91acf25'
-        ),
-        SHAPELLA_FORK_VERSION=Web3.to_bytes(hexstr=HexStr('0x0300006f')),
-        SHAPELLA_EPOCH=244224,
+        **asdict(BASE_NETWORKS[CHIADO]),
     ),
 }
